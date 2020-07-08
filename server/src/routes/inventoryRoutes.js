dab0a01ecf1ec10b2dcd1090764d6ab7272cc979
iho33 <outmatched@gmail.com>
2020-07-08 08:30:21 -0400

    added placeholder for get all inventory

--- server/src/routes/inventoryRoutes.js
+++ server/src/routes/inventoryRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const inventoryList = require('../data/inventory.json');
+const warehouseList = require('../data/locations.json');
 
+// get all inventory
+// route goes here
+
+// get a specific inventory item
 router.get('/inventory/:id', (req, res) => {
     const targetInventoryItem = inventoryList.find((object) => object.id == req.params.id);
 
@@ -13,5 +18,20 @@ router.get('/inventory/:id', (req, res) => {
       });
     }
   });
+
+// delete a specific inventory item
+router.delete('/inventory/:id', (req, res) => {
+  const targetInventoryItem = inventoryList.find((object) => object.id === req.params.id);
+  const targetInventoryItemIndex = inventoryList.findIndex((object) => object.id === req.params.id);
+
+  if (targetInventoryItem) {
+      inventoryList.splice(targetInventoryItemIndex,1);
+      res.json(warehouseList);
+  } else {
+      res.status(400).json({
+          error: 'Inventory item not found. Inventory item could not be deleted.'
+      });
+  }
+})
 
 module.exports = router;
