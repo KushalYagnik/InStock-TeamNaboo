2fd1b502bfe72950790ad78071b0444aa9f19ccf
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-08 18:38:03 -0400

    install modal

---
+++ client/src/components/WarehouseModal/WarehouseModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './warehouseModal.scss'
+import Modal from 'react-modal'
+
+export default function WarehouseModal() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
