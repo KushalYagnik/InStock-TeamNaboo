f7cb17be6323d4d5bc7b5d647b46ae14ab22451f
KushalYagnik <kushalyagnik@gmail.com>
2020-07-08 21:18:59 -0400

    Inventory page added, lists all products and relevant details from inventory.json

---
+++ client/src/components/InventoryItems/InventoryItems.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Kebab from '../InventoryKebabMenu/InventoryKebabMenu';
+import './InventoryItems.scss'
+
+export class InventoryItems extends Component {
+    state = {
+        productList: this.props.products[0]
+    }
+
+    render() {
+        const inventoryList = this.props.products[0].map(item => {
+            return (
+                <div key={item.id} className='inventory'>
+                    <Link to={`/${item.id}`} className='inventory__unit'>
+                        <div className='inventory__item'>
+                            <div className='inventory__item-label lbl'>Item</div>
+                            <div className="inventory__item-name">{item.name}</div>
+                            <div className="inventory__item-description">{item.description}</div>
+                        </div>
+                        <div className="inventory__date">
+                            <div className="inventory__date-label lbl">Last Ordered</div>
+                            <div className="inventory__date-date">{item.lastOrdered}</div>
+                        </div>
+                        <div className="inventory__location">
+                            <div className="inventory__location-label lbl">Location</div>
+                            <div className="inventory__location-city">{item.city}</div>
+                        </div>
+                        <div className="inventory__quantity">
+                            <div className="inventory__quantity-label lbl">Quantity</div>
+                            <div className="inventory__quantity-amount">{item.quantity}</div>
+                        </div>
+                        <div className="inventory__status">
+                            <div className="inventory__status-label lbl">Status</div>
+                            <div className="inventory__status-status">{item.isInstock.toString()}</div>
+                        </div>
+                    </Link>
+                    <Kebab/>
+                </div>
+            )
+        })
+        
+        return (
+            <>
+                {inventoryList}
+            </>
+        )
+    }
+}
+
+export default InventoryItems
