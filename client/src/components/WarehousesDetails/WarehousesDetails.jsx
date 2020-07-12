5037e3db007242ad98dca95cd9e70a4725eb71f3
Michael Lim <michaelelim@gmail.com>
2020-07-12 13:59:13 -0400

    Warehouses arrow contents all working

--- client/src/components/WarehousesDetails/WarehousesDetails.jsx
+++ client/src/components/WarehousesDetails/WarehousesDetails.jsx
@@ -1,66 +1,86 @@
 import React from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom'
+import Kebab from "../../assets/Icons/SVG/Icon-kebab-default.svg";
 import '../../App.scss';
 import '../WarehousesDetails/WarehousesDetails.scss';
 import LeftArrow from "../../assets/Icons/SVG/Icon-back-arrow.svg";
 
-class WarehousesDetails extends React.Component {
-  state = { 
-    warehouses: [],
-    widgets: []
-   }
+export default class WarehousesDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      warehouse: {},
+      widgets: []
+    }
+  }
 
   componentDidMount() {
     axios
     .get('/locations.json')
     .then(res => {
-      const warehouses = res.data;
-      this.setState({ warehouses })
+      console.log(res.data)
+      const filteredWarehouse = res.data.find((item, i) => {
+        return this.props.id === res.data[i].id
+      })
+      this.setState({ warehouse: filteredWarehouse })
     })
-    .catch(err => console.log("Errors: ", err))
-
+    .catch(err => console.log("Warehouse error: ", err))
+  
     axios
-    .get('/inventory.json')
-    .then(res => {
-      const widgets = res.data;
-      this.setState({ widgets })
+      .get('/inventory.json')
+      .then(res => { 
+        const filteredWidgets = res.data.filter((_item, i) => {
+          return (this.props.id === res.data[i].warehouseId)
+        })
+      this.setState({ widgets: filteredWidgets });
     })
-    .catch(err => console.log("Errors: ", err))
+    .catch(err => console.log("Widget error: ", err))
   }
 
-  render () {
+render () {
     return (
       <div className="locations-details">
-        <section className="details-container">
-          <img src={LeftArrow} className="details__left-arrow" alt="left arrow"/>
-          <div className="details__title">Warehouse Name</div>
-          <div className="details__address-title">ADDRESS</div>
-          <div className="details__address">{theWarehouse.address.street}</div>
-          <div className="details__address2">{theWarehouse.address.location}</div>
-          <div className="details__city">{theWarehouse.city}</div>
-          <div className="details__postal">{theWarehouse.postal}</div>
-          <div className="details__contact-title">CONTACT</div>
-          <div className="details__contact">{theWarehouse.contact}</div>
-          <div className="details__position">{theWarehouse.position}</div>
-          <div className="details__phone">{theWarehouse.phone}</div>
-          <div className="details__email">{theWarehouse.email}</div>
-        </section>
-        <section className="items-container">
-            <div className="items__title">ITEM</div>
-            <div className="items__name">{theItems.name}</div>
-            <div className="items__description">{theItems.description}</div>
-            <div className="items__last-ordered-title">LAST ORDERED</div>
-            <div className="items__last-ordered-date">{theItems.lastOrdered}</div>
-            <div className="items__location-title">LOCATION</div>
-            <div className="items__city">{theItems.city}, {theItems.country}</div>
-            <div className="items__quantity-title">QUANTITY</div>
-            <div className="items__quantity">{theItems.quantity}</div>
-            <div className="items__status-title">STATUS</div>
-            <div className="items__status">{theItems.isInstock}</div>
-        </section>
-        </div>
+        {
+          Object.keys(this.state.warehouse).length > 0 && this.state.warehouse.constructor === Object ?
+            <section className="details-container" key={`warehouse-${this.state.warehouse.id}`}>
+              <Link className="details__link" to="/">
+                <img src={LeftArrow} className="details__left-arrow" alt="left arrow" />
+              </Link>
+              <div className="details__title">Warehouse Name</div>
+              <div className="details__titles">ADDRESS</div>
+              <div className="details__text">{this.state.warehouse.address.street}</div>
+              <div className="details__text">{this.state.warehouse.address.location}</div>
+              <div className="details__text">{this.state.warehouse.city}</div>
+              <div className="details__text">{this.state.warehouse.postal}</div>
+              <div className="details__titles">CONTACT</div>
+              <div className="details__text">{this.state.warehouse.contact.name}</div>
+              <div className="details__text">{this.state.warehouse.contact.position}</div>
+              <div className="details__phone">{this.state.warehouse.contact.phone}</div>
+              <div className="details__text">{this.state.warehouse.contact.email}</div>
+            </section> : null
+        }
+        {
+          this.state.widgets.length > 0 ? 
+            this.state.widgets.map(item => 
+              <section className="items-container" key={`widget-${item.name}`}>
+                <img src={Kebab} className="details__kebab" alt="kebab"/>
+                <div className="details__titles">ITEM</div>
+                <div className="details__name">{item.name}</div>
+                <div className="details__text">{item.description}</div>
+                <div className="details__titles">LAST ORDERED</div>
+                <div className="details__text">{item.lastOrdered}</div>
+                <div className="details__titles">LOCATION</div>
+                <div className="details__text">{item.city}, {item.country}</div>
+                <div className="details__titles">QUANTITY</div>
+                <div className="details__text">{item.quantity}</div>
+                <div className="details__titles">STATUS</div>
+                <div className="details__text">{item.isInstock}</div>
+              </section>
+            ) : null
+        }
+      </div>  
     )
   }
 }
-
-export default WarehousesDetails;
